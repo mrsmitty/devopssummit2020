ec71a03dbec1ef8c18f38d5ae17b2fe7ae89a344
Paul Smithdale <Paul.Smithdale@insight.com>
2020-03-02 08:52:01 +0800

    Fix test

--- src/devops-summit/src/app/app.component.spec.ts
+++ src/devops-summit/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('devops-summit  I\'ve changed the app!');
+    expect(compiled.querySelector('.content span').textContent).toContain('devops-summit I\'ve changed the app!');
   });
 });
