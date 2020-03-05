885e6a3f7935fa9e4bf4b3b498eeea28459c2dbe
Paul Smithdale <pwsjunk@gmail.com>
2020-03-05 22:20:30 +0800

    infrastructure (#3)
    * src and build
    
    * updated build
    
    * angular
    
    * build path
    
    * update tests and build
    
    * update trigger
    
    * change again
    
    * testing node cache
    
    * fix unit test
    
    * removed push trigger
    
    * Fix test
    
    * empty template
    
    * AB#306
    
    * docker build
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * infrastructure
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * Update build.yml for Azure Pipelines
    
    * only on master

--- src/devops-summit/src/app/app.component.spec.ts
+++ src/devops-summit/src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('devops-summit');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('devops-summit I\'ve changed the app!');
-  });
 });
